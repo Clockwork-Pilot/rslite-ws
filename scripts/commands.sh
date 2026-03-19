af72ee8fee488380cc113e88141d7f068445cc2b
Yaroslav Litvinov <yaroslav.litvinov@gmail.com>
2026-03-19 18:22:41 +0100

    add command  ext_fts5 __va_list_tag

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,7 +1,8 @@
 python scripts/deduplicate_struct.py sqlite_base sqlite3_uint64 crust-sqlite/crates/sqlite-base/src/sqlite3_types.rs crust-sqlite/
 python scripts/deduplicate_struct.py sqlite_base sqlite3_index_constraint crust-sqlite/crates/sqlite-base/src/sqlite3_index_constraint.rs crust-sqlite/
+python scripts/deduplicate_struct.py sqlite_base sqlite3_index_info crust-sqlite/crates/sqlite-base/src/sqlite3_index_info.rs crust-sqlite/
+python deduplicate_struct.py ext_fts5 __va_list_tag ../crust-sqlite/crates/ext-fts5/src/va_list_tag.rs ../crust-sqlite/
 
-# python scripts/deduplicate_struct.py sqlite_base sqlite3_index_info crust-sqlite/crates/sqlite-base/src/sqlite3_index_info.rs crust-sqlite/
 
 
 
