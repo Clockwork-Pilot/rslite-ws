dc84b146a4adc469c54193a9524486172620bce9
Yaroslav Litvinov <yaroslav.litvinov@gmail.com>
2026-03-19 17:09:05 +0100

    upd dedup script

---
+++ scripts/README.txt
@@ -0,0 +1,10 @@
+# Example of moving particular struct
+See commands.sh
+
+python deduplicate_struct.py MyStruct src/common/my_struct.rs src/
+
+pip install tree_sitter
+pip install tree-sitter-rust
+
+git clone https://github.com/tree-sitter/tree-sitter-rust
+
---
+++ scripts/commands.sh
@@ -0,0 +1,7 @@
+python scripts/deduplicate_struct.py sqlite_base sqlite3_uint64 crust-sqlite/crates/sqlite-base/src/sqlite3_types.rs crust-sqlite/
+python scripts/deduplicate_struct.py sqlite_base sqlite3_index_constraint crust-sqlite/crates/sqlite-base/src/sqlite3_index_constraint.rs crust-sqlite/
+
+# python scripts/deduplicate_struct.py sqlite_base sqlite3_index_info crust-sqlite/crates/sqlite-base/src/sqlite3_index_info.rs crust-sqlite/
+
+
+
