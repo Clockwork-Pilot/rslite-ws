d055ae8eef31f3605ad20313ffd6b0bcce3ded22
YuraLitvinov <litvinov.yura@gmail.com>
2026-03-29 12:15:39 +0200

    synchronize porting and filter

--- run-docker-porting.sh
+++ run-docker-porting.sh
@@ -35,17 +35,21 @@ else
     ENTRYPOINT_CMD="claude --dangerously-skip-permissions --model $MODEL --plugin-dir /plugin"
 fi
 
+
+
 CMD=(bash -c "/docker-scripts/porting/user-porting-entrypoint.sh ; $ENTRYPOINT_CMD")
 
 docker run -it \
     -e PORTING_FILE \
+    -e CONTEXT_SEED=/context_seed.json \
+    -e WORK_DIR=/x/y/z \
     -e WORKSPACE_ROOT=/workspace \
     -e CLAUDE_PROJECT_ROOT=/workspace \
     -e CLAUDE_PLUGIN_ROOT=/plugin \
     -v $(pwd)/ra_ap_shell:/ra_ap_shell:Z \
     -v $(pwd)/claude-plugin:/plugin:ro,Z \
     -v $(pwd)/crust-sqlite:/x/y/z:Z \
-    -v $(pwd)/$JSON_FILE:/porting_file.json:ro,Z \
+    -v $(pwd)/$JSON_FILE:/context_seed.json:ro,Z \
     -v $(pwd)/crust-sqlite/$PORTING_FILE:/workspace/$PORTING_FILE:rw,Z \
     -v $(pwd)/crust_to_rust_loop:/crust_to_rust_loop:ro,Z \
     layered-sqlite-crust "${CMD[@]}"
